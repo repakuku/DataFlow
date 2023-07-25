0af6f23b8b5737aa6ff859a996716d723a57eb0f
Alexey Turulin <repakuku@icloud.com>
2023-07-24 20:13:01 -0400

    Initial Commit

---
+++ DataFlow/DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowApp.swift
+//  DataFlow
+//
+//  Created by Алексей Турулин on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
